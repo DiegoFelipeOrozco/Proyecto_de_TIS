a31ba708fa356b5961da3ff1e253a7f352f946b2
Diego Felipe Orozco Penagos <Dioro1999@gmail.com>
2019-09-29 22:07:29 -0500

    primer acercamiento al test de ListarTareas

--- Implementacion/agendador/App.js
+++ Implementacion/agendador/App.js
@@ -45,7 +45,15 @@ const App: () => React$Node = () => {
   return (
     <View style={styles.container}>
       <Header/>
-      <ListaTareas/>
+      <ListaTareas tareas={[{key:'0', name:'Mercar', horaI:"8:30",horaF:"9:00"},
+                {key:'1', name:'Estudiar para el Quiz', horaI:"10:30",horaF:"0"},
+                {key:'2', name:'Ir al GYM', horaI:"12:50",horaF:"13:30"},
+                {key:'3', name:'Tarea de Electronica', horaI:"12:50",horaF:"13:30"},
+                {key:'4', name:'Estudiar IoT', horaI:"13:50",horaF:"14:30"},
+                {key:'5', name:'Almorzar', horaI:"14:40",horaF:"16:30"},
+                {key:'6', name:'Recoger a Lupe', horaI:"17:20",horaF:"18:50"},
+                {key:'7', name:'Comprar comida del Perro', horaI:"20:00",horaF:"21:30"},
+                {key:'8', name:'Lavar Ropa', horaI:"21:30",horaF:"21:58"}]}/>
     </View>
   );
 };
---
+++ Implementacion/agendador/__tests__/ListaTareas-test.js
@@ -0,0 +1,28 @@
+import 'react-native';
+import React from 'react';
+import ListaTareas from '../src/component/listaTareas';
+import { fireEvent, nativeEvent, render } from '@testing-library/react-native';
+
+test('lista las tareas', async ()=>{
+	tareas = [{key:'0', name:'Mercar', horaI:"8:30",horaF:"9:00"},
+                {key:'1', name:'Estudiar para el Quiz', horaI:"10:30",horaF:"0"},
+                {key:'2', name:'Ir al GYM', horaI:"12:50",horaF:"13:30"},
+                {key:'3', name:'Tarea de Electronica', horaI:"12:50",horaF:"13:30"},
+                {key:'4', name:'Estudiar IoT', horaI:"13:50",horaF:"14:30"},
+                {key:'5', name:'Almorzar', horaI:"14:40",horaF:"16:30"},
+                {key:'6', name:'Recoger a Lupe', horaI:"17:20",horaF:"18:50"},
+                {key:'7', name:'Comprar comida del Perro', horaI:"20:00",horaF:"21:30"},
+                {key:'8', name:'Lavar Ropa', horaI:"21:30",horaF:"21:58"}];
+
+    var stringToMatch = "(";
+    for (var i = 0; i < tareas.length; i++) {
+    	stringToMatch += tareas[i].name + "|";
+    }
+    stringToMatch = stringToMatch.slice(0, -1) + ")";
+
+	const { queryAllByText, debug } = render(<ListaTareas tareas={tareas}/>);
+
+	const textos = queryAllByText(new RegExp(stringToMatch, 'i'));
+
+	expect(textos.length).toBe(tareas.length);
+});
